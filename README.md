8399029f0814bf9b07f753ba0fce33b3535ea90e
mervin <mydiamervin@gmail.com>
2020-03-03 21:03:15 +0800

    update

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# knowledge
-knowledge
+<!--
+Created: Sat Nov 16 2019 17:06:10 GMT+0800 (China Standard Time)
+Modified: Sat Nov 16 2019 20:56:20 GMT+0800 (China Standard Time)
+-->
+# asd
+``` C
+$$
+```
--- tools/rename.js
+++ tools/rename.js
@@ -1,17 +1,23 @@
 const fs = require('fs');
+var callfile = require('child_process');
 
-fs.readdir('./codes', function (err, files) {
-    // console.log(files)
+fs.readdir('../codes', function (err, files) {
+    const resultArr = [];
     files.forEach(filename => {
         let oldname = './codes/' + filename;
-        const reg = /(?:(\d{4})(0[0-9]|1[0-2])(3[0-1]|[0-2][0-9]))0([\S\s]+)\[([\S\s]+)\]\.md/
-        const as = filename.match(reg)[5].toUpperCase()
-        let newname = './codes/' + filename.replace(reg, `$1$2$30$4[${as}].md`)
-        // console.log(newname)
-        fs.rename(oldname, newname, err => {
-            if (!err) {
-                console.log(filename + 'success')
-            }
-        })
+        const engNameReg = /(?:(\d{4})(0[0-9]|1[0-2])(3[0-1]|[0-2][0-9]))\d([\w\-\'\:]+)\[([0-9A-Z-]+)\]\.md/g
+        const reg = /(?:(\d{4})(0[0-9]|1[0-2])(3[0-1]|[0-2][0-9]))\d([\S\s]+)\[([\S\s]+)\]\.md/
+        if (!engNameReg.test(filename)) {
+            resultArr.push(filename)
+        }
     })
+    if (resultArr.length > 0) {
+        console.log('以下文件名存在问题:')
+        resultArr.forEach(e => {
+            console.log(e)
+        })
+    } else {
+        callfile.execFile('./update.sh', [], function (err) { console.log(err) })
+    }
 })
+
